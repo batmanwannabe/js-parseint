7fd4a21574a17b71f089de3f2906624b83e318e8
solomon911 <solomonsahayaraj911@gmail.com>
2020-07-07 07:21:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-lwxwsw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-lwxwsw)
---
+++ index.js
@@ -0,0 +1,8 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript codlet e!
+const appDiv = document.getElementById('app');
+let text = 10
+let int = parseInt(text, 10)
+appDiv.innerHTML = `<h1>${int}</h1>`;
